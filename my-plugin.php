0e6b88825e03bfb894f0d37952e1a92ff30b2c67
bisnukundu <bisnukundu@gmail.com>
2023-09-12 01:54:37 +0600

    after active and deactive or delete plugin the database table will create delete and truncate

--- my-plugin.php
+++ my-plugin.php
@@ -9,3 +9,28 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
+//After Active the plugin, the table will be create;
+function after_activation(){
+	global $wpdb, $table_prefix;
+	$table_name = $table_prefix . 'bisnu';
+	$query      = "CREATE TABLE $table_name (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  phone VARCHAR(255))";
+	$wpdb->query( $query );
+
+	$wpdb->insert( $table_name, [ 'name' => "Bisnu", 'email' => 'bisnu@gmail.com', 'phone' => '+880174446' ] );
+}
+
+register_activation_hook( __FILE__, 'after_activation' );
+
+
+//After Deactive the plugin, the table will be truncate
+function after_deactivation() {
+	global $wpdb, $table_prefix;
+	$table_name = $table_prefix . 'bisnu';
+	$wpdb->query( "TRUNCATE TABLE $table_name" );
+}
+
+register_deactivation_hook( __FILE__, 'after_deactivation' );
--- uninstall.php
+++ uninstall.php
@@ -2,3 +2,9 @@
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die();
 }
+
+//After uninstall the plugin all database table will remove related with this plugin
+
+global $wpdb, $table_prefix;
+$table_name = $table_prefix . 'bisnu';
+$wpdb->query( "DROP TABLE $table_name" );
